a8570a4d4503de10119c0c6668504084f083073b
ljc <ljincheng@126.com>
2021-04-03 13:01:27 +0800

    完善拖拽显示效果

---
+++ src/core/Animation.js
@@ -0,0 +1,11 @@
+(function() {
+
+    var startAnim=function(arg){
+        var duration = arg.duration || 1000;
+        var action=arg.action;
+
+    };
+
+    geomap.Animation={startAnim:startAnim};
+
+})();
--- src/geo/layer/TileLayer.js
+++ src/geo/layer/TileLayer.js
@@ -22,6 +22,80 @@
        initialize: function( options) {
         options || (options = { }); 
         this._setOptions(options); 
+        this.on("initLayer",this.OnInitLayer.bind(this));
+      }, 
+      OnInitLayer:function(){
+        var canvas_copy=geomap.util.element.create("canvas",{},{zIndex:2,position:"absolute",top:"0px"});
+        const ctx_copy=canvas_copy.getContext("2d");
+        this._canvas_copy=canvas_copy;
+        this._ctx_copy=ctx_copy;
+        var size=this._map.getSize();
+        this._canvas_copy.width=size.x;
+        this._canvas_copy.height=size.y;
+        this._canvas_copy.style.width=size.x+"px";
+        this._canvas_copy.style.height=size.y+"px";
+      },
+      OnDragStart:function(arg){
+        this._dragStartPos=arg.point;
+        this._ctx_copy.clearRect(0,0,this.width,this.height);
+        this._ctx_copy.drawImage(this._layerCanvas,0,0);
+      },
+      OnDrag:function(arg){
+        if(arg.boundsChanged){
+          this._drawStart=arg.point.subtract(this._dragStartPos);
+          this.ViewReset2();
+          this.fire("drawCanvas");
+        }
+      },
+      OnDragEnd:function(arg){
+        this.ViewReset();
+        this._ctx_copy.clearRect(0,0,this.width,this.height);
+       },
+      drawingCanvas:function(ctx,options){
+        var map=this._map,
+          p0 = (this._drawStart || new Point(0,0)).round(),
+         scale=(this._canvasScale || 1),
+          size=map.getSize(),
+         box=size._multiplyBy(scale).round(),
+         p1=map.toTransformScreen(p0,1-scale).round();
+          // ctx.drawImage(this._layerCanvas,p1.x,p1.y,box.x,box.y);
+        ctx.drawImage(this._canvas_copy,p1.x,p1.y,box.x,box.y);
+         ctx.drawImage(this._layerCanvas,0,0,size.x,size.y);
+      } ,
+      ViewReset2:function(){ 
+        // this._drawStart && this._drawStart.zero();
+        // this._canvasScale=1;
+        var map=this._map,
+            z=map.zoom,
+            tsize=map.tileSize,
+            bounds=map.getBounds(),
+            res=map.resolution(z),
+            lock=this._drawLock+1;
+           
+        var cells=Math.round(this.width/tsize)+2;
+        var rows=Math.round(this.height/tsize)+2;
+  
+        var startTile=this.OriginTileInfo(res,bounds.min);
+        var cell=startTile.cell;
+        var row=startTile.row;
+        var left=startTile.left;
+        var top=startTile.top;
+  
+        this._drawLock=lock;
+        this._layerCtx.clearRect(0,0,this.width,this.height);
+        for(var c=0;c<cells;c++){
+          for(var r=0;r<rows;r++){
+            var  x=1*cell + c;
+            var y=1*row + r;
+            var l=Math.floor(left+tsize*c);
+            var t=Math.floor(top+tsize*r);
+            if( x>=0 && y>=0 ){
+                var imgUrl=geomap.util.template(this.url,{z:z,x:x,y:y});
+                this.FromURL(imgUrl,{left:l,top:t,lock:lock});  
+            }
+          }
+        }
+        //this.fire("drawCanvas");
       },
       ViewReset:function(){ 
         this._drawStart && this._drawStart.zero();
