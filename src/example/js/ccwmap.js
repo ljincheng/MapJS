7910314e04713d7579c79469bbea836a30d88041
ljc <ljincheng@126.com>
2021-04-16 19:20:53 +0800

    完善

--- src/example/js/ccwmap.js
+++ src/example/js/ccwmap.js
@@ -36,57 +36,94 @@ else if (typeof define === 'function' && define.amd) {
             if(status==200){ 
                 var result=JSON.parse(body);
                 if(result.code == 1){
-                    initMap(this,result);
+                    loadProjectMap(this,result);
                 }
             }
             
         }.bind(map)});
-    };
+    }; 
 
-    function initMap(map,result){
-        var center,rdata=result.data,centerStr=rdata.center,zoom=rdata.zoom,maxZoom=rdata.maxZoom||5,minZoom=rdata.minZoom||0;
-                    
-        if(centerStr && centerStr.indexOf(",")>0){
-            var p=centerStr.split(",");
-            center=new geomap.Point(p[0],p[1]);
-        }else{
-            center=new geomap.Point(0,0);
+    function loadProjectMap(map,result){
+        var  rdata=result.data;
+        if(!rdata.mapInfo){
+            return;
         }
-        if(zoom === undefined){
-            zoom=1;
+        var mapInfo=rdata.mapInfo,
+         projectMap=rdata.projectMap;
+        map.ccw={projectMap:projectMap,mapInfo:mapInfo};
+        loadMapInfo(mapInfo);
+    }
+    function projectMapChange(index){
+
+        if(map.ccw.projectMap && map.ccw.projectMap.length > index){
+            var mapInfo= map.ccw.projectMap[index];
+            loadMapInfo(mapInfo);
+            map.fire("map_chage",mapInfo);
+        } 
+    }
+
+    function loadMapInfo(mapInfo){
+        map.ccw.mapInfo=mapInfo; 
+        if(!map.ccw.mapObj){
+            var center,centerStr=mapInfo.center,zoom=mapInfo.zoom,maxZoom=mapInfo.maxZoom||5,minZoom=mapInfo.minZoom||0;
+            if(centerStr && centerStr.indexOf(",")>0){
+                var p=centerStr.split(",");
+                center=new geomap.Point(p[0],p[1]);
+            }else{
+                center=new geomap.Point(0,0);
+            }
+            if(zoom === undefined){
+                zoom=1;
+            }
+            map.maxZoom=maxZoom;
+            map.minZoom=minZoom;
+            map.setCenter(center,zoom);
+            var parkingLayer=new geomap.TileLayer({url:mapInfo.mapUrl});
+            map.addLayer(parkingLayer);
+            var vectorLayer=new geomap.VectorLayer();
+            // paletteLayer.setType("Line",false);
+            map.addLayer(vectorLayer);
+            // var paletteLayer=new geomap.PaletteLayer({drawType:"Polygon"});
+            var paletteLayer=new geomap.PaletteLayer({drawType:"Rect"});
+            
+            paletteLayer.on("geometry_change",function(e){paletteGeometryChangeEvent(map,e)});
+            // paletteLayer.setType("Line",false);
+            map.addLayer(paletteLayer);
+            // var clickPolygon=new geomap.Polygon(map,{style:{fillStyle:"rgba(0, 0, 200, 0.5)",strokeStyle:"white",lineWidth:2},_fill:true,lineDash:[4,2]});
+            // paletteLayer.addGeometry(clickPolygon);
+            map.on("pointcoord",function(e){
+                var p=e.coord.x+","+e.coord.y;
+                clickParingInfoEvent(map,p);});
+            map.on("rectcoord",function(e){
+                var p=e.minx+","+e.miny+","+e.maxx+","+e.maxy;
+                clickParingInfoEvent(map,p);
+               // console.log("[ccwmap.js]rectcoord="+e.minx+","+e.miny+","+e.maxx+","+e.maxy);
+            });
+            
+            map.fire("drawmap");
+           
+    
+            //=== 测试区===
+    
+           var marker=new geomap.Marker(map,{width:200,height:120,style:{lineWidth:0}});
+           map.addGeometry(marker);
+            // map.ccw={paletteLayer:paletteLayer,clickPolygon:clickPolygon};
+            map.ccw.mapObj={parkingLayer:parkingLayer,
+                paletteLayer:paletteLayer,
+                marker:marker,
+                vectorLayer:vectorLayer,
+                deleteFeature:deleteFeature.bind(map),
+                projectMapChange:projectMapChange.bind(map)};
+                map.on("map_chage",function(){
+                    paletteLayer.clearGeometry();
+                    parkingLayer.refreshCache();
+                    vectorLayer.clearData();
+                });
+            map.fire("map_complete",map);
+        }else{ 
+            map.ccw.mapObj.parkingLayer.url=mapInfo.mapUrl;
+            map.fire("drawmap");
         }
-        map.maxZoom=maxZoom;
-        map.minZoom=minZoom;
-        map.setCenter(center,zoom);
-        var parkingLayer=new geomap.TileLayer({url:rdata.mapUrl});
-        map.addLayer(parkingLayer);
-        var vectorLayer=new geomap.VectorLayer();
-        // paletteLayer.setType("Line",false);
-        map.addLayer(vectorLayer);
-        // var paletteLayer=new geomap.PaletteLayer({drawType:"Polygon"});
-        var paletteLayer=new geomap.PaletteLayer({drawType:"Rect"});
-        
-        paletteLayer.on("geometry_change",function(e){paletteGeometryChangeEvent(map,e)});
-        // paletteLayer.setType("Line",false);
-        map.addLayer(paletteLayer);
-        // var clickPolygon=new geomap.Polygon(map,{style:{fillStyle:"rgba(0, 0, 200, 0.5)",strokeStyle:"white",lineWidth:2},_fill:true,lineDash:[4,2]});
-        // paletteLayer.addGeometry(clickPolygon);
-        map.on("pointcoord",function(e){
-            var p=e.coord.x+","+e.coord.y;
-            clickParingInfoEvent(map,p);});
-        map.on("rectcoord",function(e){
-            var p=e.minx+","+e.miny+","+e.maxx+","+e.maxy;
-            clickParingInfoEvent(map,p);
-           // console.log("[ccwmap.js]rectcoord="+e.minx+","+e.miny+","+e.maxx+","+e.maxy);
-        });
-        map.fire("drawmap");
-
-        //=== 测试区===
-
-       var marker=new geomap.Marker(map,{width:200,height:120,style:{lineWidth:0}});
-       map.addGeometry(marker);
-        // map.ccw={paletteLayer:paletteLayer,clickPolygon:clickPolygon};
-        map.ccw={parkingLayer:parkingLayer,paletteLayer:paletteLayer,marker:marker,vectorLayer:vectorLayer,deleteFeature:deleteFeature.bind(map)};
     }
 
     function paletteGeometryChangeEvent(map,geometry){
@@ -95,7 +132,7 @@ else if (typeof define === 'function' && define.amd) {
         if(geometry._coordinates.length<1){
             return;
         }
-        var mapId=map.mapId;
+        var mapId=map.ccw.mapInfo.mapId;
         var url=MAP_SERVER+"/geo/wms/addFeature/"+mapId;
         var layerId="PARKING-POLYGON-001";
         var typeName="geo_parking_polygon";
@@ -109,22 +146,22 @@ else if (typeof define === 'function' && define.amd) {
         // }
         // var bodyValue={layerId:layerId,coords:p};
         // var bodyData=geomap.util.template("layerId={layerId}&coords={coords}&building_id=b001&parking_no=车位：XXXX-002",bodyValue);
-        var bodyData={layerName:typeName,geometry:geometry.getText(),properties:{building_id:"b001",parking_no:"车位：XXXX-004"}};
+        var bodyData={layerName:typeName,geometry:geometry.getText(),properties:{building_id:"b001",parking_no:"车位：XXXX-004",sale_status:1,map_id:mapId}};
         Request(url,{method:"JSON",body:bodyData,header:REQ_HEADER,onComplete:function(xhr){
             var body=xhr.response,status=xhr.status; 
             geomap.debug("url="+url+",body="+body);
             if(status==200){ 
                 var result=JSON.parse(body);
                 if(result.code == 1){
-                    map.ccw.paletteLayer.clearGeometry();
-                    map.ccw.parkingLayer.refreshCache();
+                    map.ccw.mapObj.paletteLayer.clearGeometry();
+                    map.ccw.mapObj.parkingLayer.refreshCache();
                 }
             }
         }});
     }
     function clickParingInfoEvent(map,p){
         // var geomtry="POLYGON((0 0,0 90,90 90,90 0,0 0))";
-        var mapId=map.mapId;
+        var mapId=map.ccw.mapInfo.mapId;
         var layerId="PARKING-POLYGON-001";
         var url=MAP_SERVER+"/geo/wms/coordQuery/"+mapId;
         // var bodyValue=Extend({layerId:layerId},e.coord);
@@ -135,11 +172,11 @@ else if (typeof define === 'function' && define.amd) {
         Request(url,{method:"POST",body:bodyData,header:REQ_HEADER,onComplete:function(xhr){
             var res=xhr.response,status=xhr.status; 
             geomap.debug("url="+url+",body="+res);
-            map.ccw.vectorLayer.clearData();
+            map.ccw.mapObj.vectorLayer.clearData();
             if( status==200 && res.length>0 && res.indexOf("{")==0){ 
                 var featureCollection=JSON.parse(res);
                if(featureCollection.type="FeatureCollection"){
-                map.ccw.vectorLayer.addData(featureCollection,{style:{fillStyle:"rgba(0,0,200,0.5)",strokeStyle:"#fff",lineWidth:2},_fill:true,lineDash:[4,2]});
+                map.ccw.mapObj.vectorLayer.addData(featureCollection,{style:{fillStyle:"rgba(0,0,200,0.5)",strokeStyle:"#fff",lineWidth:2},_fill:true,lineDash:[4,2]});
                 map.fire("drawmap");
                }
                setEditContent(featureCollection);
@@ -152,7 +189,7 @@ else if (typeof define === 'function' && define.amd) {
     }
 
     function deleteFeature(fid){
-        var mapId=this.mapId;
+        var mapId=this.ccw.mapInfo.mapId;
         var layerId="PARKING-POLYGON-001";
         var url=MAP_SERVER+"/geo/wms/deleteFeature/"+mapId;
         // var bodyValue=Extend({layerId:layerId},e.coord);
@@ -165,8 +202,8 @@ else if (typeof define === 'function' && define.amd) {
             if(status==200){ 
                 var result=JSON.parse(body);
                 if(result.code == 1){
-                    map.ccw.vectorLayer.clearData();
-                    map.ccw.parkingLayer.refreshCache();
+                    map.ccw.mapObj.vectorLayer.clearData();
+                    map.ccw.mapObj.parkingLayer.refreshCache();
                 }
             }
         }});
